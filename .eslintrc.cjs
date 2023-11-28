f7bb81c5c220b1412de1520ee783b3328aba19f9
李振宇 <lizhenyu@joyware.com>
2023-11-28 15:24:42 +0800

    build: update package lastet

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: [],
+  rules: {},
+};
